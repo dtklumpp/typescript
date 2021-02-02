1414239a9b5b8218b1e13d1cc640d689ce835954
dtklumpp <dtklumpp@gmail.com>
2021-02-02 17:20:26 -0500

    basic typescript setup

---
+++ intro-typescript.js
@@ -0,0 +1,10 @@
+var message = "Hello World";
+console.log(message);
+function sayHello() {
+    console.log('hello');
+    // return 12
+}
+;
+function sum(x, y) {
+    return x + y;
+}
---
+++ intro-typescript.ts
@@ -0,0 +1,12 @@
+const message: string = "Hello World"
+console.log(message)
+
+function sayHello(): void {
+	console.log('hello');
+    // return 12
+};
+
+function sum(x: number, y: number): number {
+    return x + y;
+}
+
